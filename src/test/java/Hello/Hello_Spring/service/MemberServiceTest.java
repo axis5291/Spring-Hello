aa143e69810a5dd43c9118ac4adb6b6d8ed5a729
AxisMac <erlia@naver.com>
2025-02-20 19:48:57 +0900

    6

--- src/test/java/Hello/Hello_Spring/service/MemberServiceTest.java
+++ src/test/java/Hello/Hello_Spring/service/MemberServiceTest.java
@@ -15,10 +15,14 @@ public class MemberServiceTest {
         member.setName("마종호");
 
         //when
-        Long saveId=memberService.join(member);
+        Long saveId=memberService.join(member);  //MemberService에서 레포지토리에 넘김
         //then
         Member findMember=memberService.findOne(saveId).get();  //Optional에서 값을 꺼낼때는 get()메서드를 사용
         Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+        //생성한 객체의 이름과 찾아온 객체의 이름이 같은지 확인
+        if(member.getName().equals(findMember.getName())){
+            System.out.println("같은 회원으로 판명되었습니다.");
+        }
     }
     @Test
     void testFindMembers() {
