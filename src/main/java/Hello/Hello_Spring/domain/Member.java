1fa5b03f344afb2e01ed20bdca75c9e526a826bd
Axis <erlia@naver.com>
2025-03-08 07:13:38 +0900

    17

--- src/main/java/Hello/Hello_Spring/domain/Member.java
+++ src/main/java/Hello/Hello_Spring/domain/Member.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+// 비즈니스 관점에서는 Member와 같은 클래스를 도메인 모델이라고 부르고, JPA 관점에서는 그 클래스가 데이터베이스와 매핑되는 객체이므로 엔티티 클래스라고 부릅니다.
+// 결국 둘은 같은 객체를 의미할 수 있지만, 그 관점에 따라 다르게 불리는 것입니다.
+
 @Entity  //JPA를 쓸려면 이렇게 엔티티로 관리한다고 알려줘야함
 public class Member {
     @Id
